62483106136fc241fdbad4de476e589de70013cb
Camilo  Hernandez <camilo.hernandezmarin@jumbo.com>
2020-03-18 09:33:53 +0100

    force trans management

--- src/main/java/com/optimisticlock/demo/api/ConfigTransaction.java
+++
@@ -1,9 +0,0 @@
-package com.optimisticlock.demo.api;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-@Controller
-@EnableTransactionManagement
-public class ConfigTransaction {
-}
